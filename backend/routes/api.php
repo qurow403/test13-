2328269f88f0f5751294690cc1e2333b1bf0c4f7
qurw403 <qurow403@gmail.com>
2025-12-30 06:42:13 +0900

    api.php編集

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/health', function () {
-    return response()->json(['status' => 'ok']);
-});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
