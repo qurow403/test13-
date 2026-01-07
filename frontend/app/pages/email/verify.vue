b8a6fc15ced31456eb4107a55980138d896ac340
qurw403 <qurow403@gmail.com>
2026-01-07 10:27:36 +0900

    add email verification UI page

---
+++ frontend/app/pages/email/verify.vue
@@ -0,0 +1,23 @@
+<script setup>
+definePageMeta({
+    layout: 'auth'
+})
+
+const resend = async () => {
+    await $fetch('/api/email/resend', { method: 'POST' })
+    alert('認証メールを再送しました')
+}
+</script>
+
+<template>
+<div class="verify">
+    <p>登録していただいたメールアドレスに認証メールを送付しました。</p>
+    <p>メール認証を完了してください。</p>
+
+    <a href="https://mail.google.com" target="_blank" class="primary">
+        認証はこちらから
+    </a>
+
+    <button class="link" @click="resend">認証メールを再送する</button>
+</div>
+</template>
